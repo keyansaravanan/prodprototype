cc24d87f06c5838cf8b582781521e03cfdedddf8
Karthi Keyan <70126510+keyansaravanan@users.noreply.github.com>
2021-12-17 13:39:34 +0530

    Update combinedone.py

--- combinedone.py
+++ combinedone.py
@@ -199,8 +199,8 @@ def start_webcam(threadname):
     connection = server_socket.accept()[0].makefile('rb')
     vid = cv2.VideoCapture(0
     '''
-    #vid1=cv2.VideoCapture(0)
-    #vid2=cv2.VideoCapture(1)
+    vid1=cv2.VideoCapture(0)
+    vid2=cv2.VideoCapture(1)
     while True:
         #image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
 
@@ -236,27 +236,27 @@ def start_webcam(threadname):
         ret, im2 = vid.read()
         '''
 
-        #rval1, im1 = vid1.read()
-        #rval2, im2 = vid2.read()
+        rval1, im1 = vid1.read()
+        rval2, im2 = vid2.read()
         import config
         print("db1:",config.db1)
         print("db1:",config.db2)
         print("nc1:",config.nc1)
         print("nc2:",config.nc2)
         if(config.cameraoneon==True and config.cameratwoon==True):
-            im1=cv2.imread('./speakerone.jpg')
-            im2=cv2.imread('./speakertwo.jpg')
+            #im1=cv2.imread('./speakerone.jpg')
+            #im2=cv2.imread('./speakertwo.jpg')
             images_1_2_h = np.hstack((im1, im2))
             cv2.imshow('Video',images_1_2_h)
             #print("thisconditionworks")
             i=0
         if(config.cameraoneon==True and config.cameratwoon==False):
-            im1=cv2.imread('./speakerone.jpg')
+            #im1=cv2.imread('./speakerone.jpg')
             cv2.imshow('Video',im1)
             #print("thisconditionworkstwo")
             i=1
         if(config.cameratwoon==True and config.cameraoneon==False):
-            im2=cv2.imread('./speakertwo.jpg')
+            #im2=cv2.imread('./speakertwo.jpg')
             #print("thisconditionworksthree")
             cv2.imshow('Video',im2)
             i=2
