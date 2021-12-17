db8a853923920bf1e3503312a3ea32cb04414f8f
Karthi Keyan <70126510+keyansaravanan@users.noreply.github.com>
2021-12-17 22:32:30 +0530

    Update secondaudiotest.py

--- secondaudiotest.py
+++ secondaudiotest.py
@@ -2,7 +2,25 @@ from threading import Thread
 import time
 from combinedone import start_recording1
 from combinedone import start_recording2
+from combinedone import start_webcam
+from combineoftwo import hello
+from combineoftwo import helloone
+from combinedone import decisionblockforcameraone
+from combinedone import decisionblockforcameratwo
+
+thread1 = Thread(target=start_recording1, args=("Thread-1", ))
+thread2 = Thread(target=start_recording2, args=("Thread-2", ))
+thread3 = Thread(target=hello, args=("Thread-3", ))
+thread4 = Thread(target=helloone, args=("Thread-4", ))
+thread5 = Thread(target=decisionblockforcameraone, args=("Thread-5", ))
+thread6 = Thread(target=decisionblockforcameratwo, args=("Thread-6", ))
+thread7 = Thread(target=start_webcam, args=("Thread-7", ))
 
 
-thread5 = Thread(target=start_recording2, args=("Thread-2", ))
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
 thread5.start()
+thread6.start()
+thread7.start()
